82a8339e28f21ae2629a32a4718b9c6ef09894f8
suvaneeth <suvaneeth@gmail.com>
2016-02-08 09:53:18 +0300

    test

---
+++ TheClinicApp/UIClasses/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheClinicApp.UIClasses
+{
+    public class Messages
+    {
+        public static string LoginFailed
+        {
+            get { return "User Name / Password wrong !"; }
+        }
+    }
+}
