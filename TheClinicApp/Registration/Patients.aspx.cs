286ac7c021bf0a4d4914ec5bba3e40980a0085df
tvm-thomson <thomson.varkey@thrithvam.me>
2016-02-16 11:33:41 +0530

    design changes

--- TheClinicApp/Registration/Patients.aspx.cs
+++ TheClinicApp/Registration/Patients.aspx.cs
@@ -107,5 +107,18 @@ namespace TheClinicApp.Registration
              
         }
 
+        protected void ImgBtnUpdate_Command1(object sender, CommandEventArgs e)
+        {
+            string[] Patient = e.CommandArgument.ToString().Split(new char[] { ',' });
+            Guid PatientID = Guid.Parse(Patient[0]);
+            txtName.Text = Patient[1];
+            txtSex.Text = Patient[6];
+            txtAge.Text = Patient[5];
+            txtAddress.Text = Patient[2];
+            txtMobile.Text = Patient[3];
+            txtEmail.Text = Patient[4];
+            txtMarital.Text = Patient[7];
+        }
+
     }
 }
