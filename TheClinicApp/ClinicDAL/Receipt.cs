adf3bce523d231e1ef42e3c27d98dbf61217e4a1
tvm-gibin <gibin.jacob@thrithvam.me>
2016-02-18 09:51:36 +0530

    add receipt.cs

---
+++ TheClinicApp/ClinicDAL/Receipt.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheClinicApp.ClinicDAL
+{
+    public class Receipt
+    {
+        #region Property
+        public string UniqueID
+        {
+            get;
+            set;
+        }
+        public string ReceiptID
+        {
+            get;
+            set;
+        }
+        public string ClinicID
+        {
+            get;
+            set;
+        }
+        public string MedicineID
+        {
+            get;
+            set;
+        }
+        public int  Unit
+        {
+            get;
+            set;
+        }
+        public string CreatedBy
+        {
+            get;
+            set;
+        }
+        public DateTime CreatedDate
+        {
+            get;
+            set;
+        }
+        public string UpdatedBy
+        {
+            get;
+            set;
+        }
+        public DateTime UpdatedDate
+        {
+            get;
+            set;
+        }
+
+        #endregion Property
+
+        #region Methods
+        #endregion Methods
+
+    }
+
+    public class ReceiptDetails
+    {
+        #region Property
+
+        #endregion Property
+
+        #region Methods
+        #endregion Methods
+
+
+    }
+
+}
