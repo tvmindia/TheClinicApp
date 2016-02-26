56adba444aa5a165b3548ba489786b9fad1ac8a4
tvm-gibin <gibin.jacob@thrithvam.me>
2016-02-26 13:56:35 +0530

    Add changes in Token and inventoryOut

--- TheClinicApp/Stock/InventoryOut.aspx.cs
+++ TheClinicApp/Stock/InventoryOut.aspx.cs
@@ -18,20 +18,32 @@ namespace TheClinicApp.Stock
         ErrorHandling eObj = new ErrorHandling();
         IssueHeaderDetails ihd = new IssueHeaderDetails();
         IssueDetails idt = new IssueDetails();
+        Stocks stok = new Stocks();
+
 
 
 
         protected void Page_Load(object sender, EventArgs e)
         {
-              
 
+            if (!IsPostBack)
+            {
 
+                DataSet ds = stok.ViewMedicines();
+
+                ddlMedicine.DataSource = ds.Tables[0];
+                ddlMedicine.DataValueField = "MedicineID";
+                ddlMedicine.DataTextField = "Name";
+                ddlMedicine.DataBind();
+
+            }
         }
 
  
 
         protected void btnIssue_Click(object sender, EventArgs e)
         {
+
 
         }
     }
