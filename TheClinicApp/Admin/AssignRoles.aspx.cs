87dc66393a738838b17a3e0d8f383c26e3480048
tvm-shamila <shamila.tp@thrithvam.me>
2016-02-29 11:46:13 +0530

    clientID passed to user page

--- TheClinicApp/Admin/AssignRoles.aspx.cs
+++ TheClinicApp/Admin/AssignRoles.aspx.cs
@@ -1,22 +1,31 @@
-﻿using System;
+﻿
+#region CopyRight
+
+//Author      : SHAMILA T P
+//Created Date: Feb-29-2016
+
+#endregion CopyRight
+
+#region Included Namespaces
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+#endregion Included Namespaces
+
 namespace TheClinicApp.Admin
 {
     public partial class AssignRoles : System.Web.UI.Page
     {
+        #region Page Load
         protected void Page_Load(object sender, EventArgs e)
         {
-
-
-
-
-
-
         }
+
+        #endregion Page Load
     }
 }
