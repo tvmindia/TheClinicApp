610640ffd8562e836adfb10a46ede54e5d18a0f9
tvm-shamila <shamila.tp@thrithvam.me>
2016-03-15 11:22:05 +0530

    Implemented autofill grid

--- TheClinicApp/AutofillGrid.aspx.cs
+++ TheClinicApp/AutofillGrid.aspx.cs
@@ -8,10 +8,16 @@ using System.Data;
 using System.Data.SqlClient;
 using TheClinicApp.ClinicDAL;
 
+using System.Web.Services;
+using System.Configuration;
+
+
+
 namespace TheClinicApp
 {
     public partial class AutofillGrid : System.Web.UI.Page
     {
+        private static int PageSize = 2;
 
         #region Global Variables
 
@@ -21,48 +27,113 @@ namespace TheClinicApp
 
         #endregion Global Variables
 
-
-
         #region Methods
 
         #region Bind Gridview
 
-        public void BindGridview()
-        {
-            DataSet dsMedicines = new DataSet();
-            stockObj.ClinicID = "2C7A7172-6EA9-4640-B7D2-0C329336F289";
-            //dsMedicines = stockObj.SearchMedicineStock(DBNull.Value);
-            dtgMedicines.DataSource = dsMedicines;
-            dtgMedicines.DataBind();
-        }
+        //public void BindGridview()
+        //{
+        //    DataSet dsMedicines = new DataSet();
+        //    stockObj.ClinicID = "2C7A7172-6EA9-4640-B7D2-0C329336F289";
+        //    dsMedicines = stockObj.SearchMedicineStock(string.Empty);
+        //    dtgMedicines.DataSource = dsMedicines;
+        //    dtgMedicines.DataBind();
+        //}
 
         #endregion Bind Gridview
 
 
-        [System.Web.Services.WebMethod]
-        public static string GetCurrentTime(string name)
-        {
-            return "Hello " + name + Environment.NewLine + "The Current Time is: "
-                    + DateTime.Now.ToString();
-         
-        }
-
-
-
-
         #endregion Methods
 
+        private void BindData()
+        {
+            DataSet dsMedicines = new DataSet();
+            stockObj.ClinicID = "2C7A7172-6EA9-4640-B7D2-0C329336F289";
+            dsMedicines = stockObj.SearchMedicineStock(string.Empty); 
+            DataTable dt = dsMedicines.Tables[0];
 
+            //gvDetails.DataSource = dt;
+            //gvDetails.DataBind();
 
+        }
+
+      
         protected void Page_Load(object sender, EventArgs e)
         {
+            //txt.Attributes.Add("onkeyup", "javascript:setTimeout('__doPostBack(\'txt\',\'\')', 0)");
             //BindGridview();
+      
+            if (!IsPostBack)
+            {
+                BindDummyRow();
+            }
+
         }
 
-        protected void txtSearch_TextChanged(object sender, EventArgs e)
+      
+        private void BindDummyRow()
+        {
+            DataTable dummy = new DataTable();
+            dummy.Columns.Add("MedicineName");
+            dummy.Columns.Add("CategoryName");
+            dummy.Columns.Add("MedicineCode");
+            dummy.Rows.Add();
+            gvMedicines.DataSource = dummy;
+            gvMedicines.DataBind();
+        }
+
+
+        [WebMethod]
+        public static string GetMedicines(string searchTerm, int pageIndex)
         {
 
+            string query = "[ViewAndFilterMedicine]";
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@SearchTerm", searchTerm);
+            cmd.Parameters.AddWithValue("@PageIndex", pageIndex);
+            cmd.Parameters.AddWithValue("@PageSize", PageSize);
+            cmd.Parameters.Add("@RecordCount", SqlDbType.Int, 4).Direction = ParameterDirection.Output;
+
+            var w = GetData(cmd, pageIndex).GetXml();
+            return w;
         }
+
+        private static DataSet GetData(SqlCommand cmd, int pageIndex)
+        {
+           
+            string strConnString = ConfigurationManager.ConnectionStrings["ClinicAppConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(strConnString))
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.Connection = con;
+                    sda.SelectCommand = cmd;
+                    using (DataSet ds = new DataSet())
+                    {
+                        sda.Fill(ds, "Medicines");
+                        DataTable dt = new DataTable("Pager");
+                        dt.Columns.Add("PageIndex");
+                        dt.Columns.Add("PageSize");
+                        dt.Columns.Add("RecordCount");
+                        dt.Rows.Add();
+                        dt.Rows[0]["PageIndex"] = pageIndex;
+                        dt.Rows[0]["PageSize"] = PageSize;
+                        dt.Rows[0]["RecordCount"] = cmd.Parameters["@RecordCount"].Value;
+                        ds.Tables.Add(dt);
+                        return ds;
+                    }
+                }
+            }
+        }
+
+       
+
+
+
+
+
+
 
     }
 }
