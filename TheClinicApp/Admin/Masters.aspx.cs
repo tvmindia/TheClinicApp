3230e0631adfe9e49aa89f29d88e3c0c2d9d0c2d
tvm-thomson <thomson.varkey@thrithvam.me>
2016-03-31 14:32:32 +0530

    with default page

--- TheClinicApp/Admin/Masters.aspx.cs
+++ TheClinicApp/Admin/Masters.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +10,54 @@ namespace TheClinicApp.Admin
 {
     public partial class Masters : System.Web.UI.Page
     {
+        ClinicDAL.Master MasterObj = new ClinicDAL.Master();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+       
+
+        protected void btclinic_ServerClick(object sender, EventArgs e)
+        {
+            MasterObj.GroupID = Guid.Parse(DropDownList1.SelectedValue);
+            MasterObj.ClinicName=txtClinic.Value;
+            MasterObj.ClinicAddress=txtaddress.Value;
+            MasterObj.ClinicLocation=txtLocation.Value;
+            MasterObj.ClinicPhone=txtPhone.Value;
+            MasterObj.InsertClinics();
+        }
+
+        protected void btDoctor_ServerClick(object sender, EventArgs e)
+        {
+            MasterObj.DoctorName=txtDoctor.Value;
+            MasterObj.DoctorPhone=txtDrPhone.Value;
+            MasterObj.DoctorEmail=txtEmail.Value;
+            MasterObj.InsertDoctors();
+        }
+
+        protected void btGroup_ServerClick(object sender, EventArgs e)
+        {
+             MasterObj.GroupName=txtGroup.Value;
+             byte[] ImageByteArray = null;
+             ImageByteArray = ConvertImageToByteArray(FileUpload1);
+             MasterObj.Logo = ImageByteArray;
+             MasterObj.InsertGroups();
+        }
+
+        private byte[] ConvertImageToByteArray(FileUpload fuImage)
+        {
+            byte[] ImageByteArray;
+            try
+            {
+                MemoryStream ms = new MemoryStream(fuImage.FileBytes);
+                ImageByteArray = ms.ToArray();
+                return ImageByteArray;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
     }
 }
--- TheClinicApp/Default.aspx.cs
+++ TheClinicApp/Default.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Security.Cryptography;
 using System.Text;
 using System.Web;
@@ -14,46 +16,122 @@ namespace TheClinicApp
 {
     public partial class Default : System.Web.UI.Page
     {
+        //ClinicDAL.User userObj = new ClinicDAL.User();
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (IsPostBack) {
-                
-                if (username.Value.ToString().Trim() !=""  )
+       
+            if (IsPostBack)
+            {
+                if (username.Value != "" && password.Value != "")
                 {
-                    UIClasses.Const constants = new UIClasses.Const();
 
-                    //if (username.Value != password.Value)
-                    //{
-                    //    password.Value = Encrypt(password.Value);
-                    //}
-
-                    //ClinicDAL.UserAuthendication UA = new ClinicDAL.UserAuthendication(username.Value, password.Value);
-
-                    ClinicDAL.UserAuthendication UA = new ClinicDAL.UserAuthendication(username.Value,password.Value);
-
-                    if (UA.ValidUser)
+                    if (username.Value.ToString().Trim() != "")
                     {
-                        if (Session[constants.LoginSession] != null)
+                        UIClasses.Const constants = new UIClasses.Const();
+
+                        //if (username.Value != password.Value)
+                        //{
+                        //    password.Value = Encrypt(password.Value);
+                        //}
+
+                        //ClinicDAL.UserAuthendication UA = new ClinicDAL.UserAuthendication(username.Value, password.Value);
+
+                        ClinicDAL.UserAuthendication UA = new ClinicDAL.UserAuthendication(username.Value, password.Value);
+
+                        if (UA.ValidUser)
                         {
-                            Session.Remove(constants.LoginSession);
+                            if (Session[constants.LoginSession] != null)
+                            {
+                                Session.Remove(constants.LoginSession);
+                            }
+
+                            Session.Add(constants.LoginSession, UA);
+                            Response.Redirect(constants.HomePage);
+
                         }
 
-                        Session.Add(constants.LoginSession, UA);
-                        Response.Redirect(constants.HomePage);
-                        
+
+
+                    }
+                    else
+                    {
+
+                        lblmsg.Text = Messages.LoginFailed;
                     }
 
-                  
-
                 }
-                else {
-
-                    lblmsg.Text = Messages.LoginFailed;
-                }
-            
             }
         }
 
-       
+        //protected void btnVerificationCode_ServerClick(object sender, EventArgs e)
+        //{
+        //    //----------*Add verification code*------------//
+        //    Random random = new Random();
+        //    int verificationCode = random.Next(1000, 10000);
+
+        //    userObj.verificationCode = verificationCode.ToString();
+        //    userObj.Email = txtEmail.Value;
+        //    userObj.AddVerificationCode();
+
+
+
+
+        //    //----------*Get verification code*------------//
+        //    userObj.Email = txtEmail.Value;
+        //    DataTable dtCode = userObj.GetUserVerificationCodeByEmailID();
+        //    verificationCode = Convert.ToInt32(dtCode.Rows[0]["VerificationCode"]);
+        //    DateTime vcCreatedTime = Convert.ToDateTime(dtCode.Rows[0]["VerificatinCreatedTime"]);
+        //    DateTime CurrentTime = DateTime.Now;
+        //    MailMessage Msg = new MailMessage();
+
+
+        //    // Sender e-mail address.
+        //    Msg.From = new MailAddress("info.thrithvam@gmail.com");
+
+
+        //    // Recipient e-mail address.
+        //    Msg.To.Add(txtEmail.Value);
+        //    string body = verificationCode.ToString();
+        //    Msg.Subject = "Verification Code  ";
+        //    Msg.Body = body;
+        //    Msg.IsBodyHtml = true;
+
+
+
+        //    // your remote SMTP server IP.
+        //    SmtpClient smtp = new SmtpClient();
+        //    smtp.Host = "smtp.gmail.com";
+        //    smtp.Port = 587;
+        //    smtp.Credentials = new System.Net.NetworkCredential("info.thrithvam", "thrithvam@2015");
+        //    smtp.EnableSsl = true;
+        //    smtp.Send(Msg);
+        //    Msg = null;
+        //}
+
+        //protected void txtVerify_ServerClick(object sender, EventArgs e)
+        //{
+        //    userObj.Email = txtEmail.Value;
+        //    DataTable dtCode = userObj.GetUserVerificationCodeByEmailID();
+        //    int verificationCode = Convert.ToInt32(dtCode.Rows[0]["VerificationCode"]);
+        //    DateTime vcCreatedTime = Convert.ToDateTime(dtCode.Rows[0]["VerificatinCreatedTime"]);
+        //    DateTime CurrentTime = DateTime.Now;
+        //    if ((CurrentTime - vcCreatedTime) < TimeSpan.FromDays(1))
+        //    {
+        //        if (verificationCode.ToString() == txtVerificationCode.Value)
+        //        {
+        //            Response.Redirect("ResetPassword.aspx");
+        //        }
+        //        else
+        //        {
+        //            lblError.Text = "Verification Code is not correct";
+        //        }
+
+        //    }
+        //    else
+        //    {
+        //        lblError.Text = "Time expired";
+        //    }
+        //}
+        
     }
 }
